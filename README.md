87cbf730f04e02efb13ffb653c3ac6c1074cac7e
Félix <the2deux@gmail.com>
2017-07-11 16:54:59 -0400

    Installed base, worked on the assets and did home page

---
+++ README.md
@@ -0,0 +1,2 @@
+Unused js files: jquery-ui.min.js, jquery-countdown.min.js, uikit.js
+Unused css files: jquery.datepick.css, jquery.treeview.css, uitkit.min.css
---
+++ assets/js/slider/nivo-active.js
@@ -0,0 +1,15 @@
+(function ($) {
+"use strict";
+/*Home main */
+	$('#mainSlider').nivoSlider({
+		directionNav: true,
+		animSpeed: 500,
+		slices: 18,
+		pauseTime: 5000,
+		pauseOnHover: false,
+		controlNav: false,
+		prevText: '',
+		nextText: ''
+	});
+
+})(jQuery);	
